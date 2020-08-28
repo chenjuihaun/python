e4aa5fb1feefb0e245cd142161c63d841da378ae
chenjuihaun <70123495+chenjuihaun@users.noreply.github.com>
2020-08-28 17:08:09 +0800

    Add files via upload

---
+++ best game ever.py
@@ -0,0 +1,86 @@
+import random
+q=1
+for i in range(9):
+    while True:
+        if q==1:
+            Q=input('c_t:')
+            if Q=='a':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
+        if q==2:
+            Q2=input('b__k:')
+            if Q2=='oo':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
+        if q==3:
+            Q3=input('bea__if_l:')
+            if Q3=='ut__u':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
+        if q==4:
+            Q4=input('cat_stro__e:')
+            if Q4=='a____ph':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
+        if q==5:
+            Q5=input('g__sip:')
+            if Q5=='oo':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
+        if q==6:
+            Q6=input('_n_ry:')
+            if Q6=='a_g':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
+        
+        if q==7:
+            Q7=input('_y_hon:')
+            if Q7=='p_t':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
+         if q==8:
+            Q8=input('yo__u_e:')
+            if Q8=='ut_b':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
+         
+        if q==9:
+            Q9=input('de_on:')
+            if Q9=='m':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
+        if q==10:
+            Q10=input('pa__ho_e:')
+            if Q10=='yp__n':
+                print('correct')
+                q=q+1
+                break
+            else:
+                print('incorrect')
