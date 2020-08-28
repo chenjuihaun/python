44838fa0b6f92f91b3ab66e59ff20f336869db6c
chenjuihaun <70123495+chenjuihaun@users.noreply.github.com>
2020-08-28 14:54:10 +0800

    Add files via upload

---
+++ 猜字遊戲.py
@@ -0,0 +1,129 @@
+q=1
+i=0
+while i<11:
+    if q==1:
+        Q=input('c_t:')
+        if Q=='a':
+            print('correct')
+            q=q+1
+            i=i+1
+            
+        else:
+            print('incorrect')
+            q=q+1
+    elif q==2:
+        Q2=input('b__k:')
+        if Q2=='oo':
+            print('correct')
+            q=q+1
+            i=i+1
+            
+        else:
+            print('incorrect')
+            q=q+1
+    elif q==3:
+        Q3=input('bea__if_l:')
+        if Q3=='ut__u':
+            print('correct')
+            q=q+1
+            i=i+1
+           
+        else:
+            print('incorrect')
+            q=q+1
+    elif q==4:
+        Q4=input('cat_stro__e:')
+        if Q4=='a____ph':
+            print('correct')
+            q=q+1
+            i=i+1
+            
+        else:
+            print('incorrect')
+            q=q+1
+    elif q==5:
+        Q5=input('g__sip:')
+        if Q5=='oo':
+            print('correct')
+            q=q+1
+            i=i+1
+         
+        else:
+            print('incorrect')
+            q=q+1
+    elif q==6:
+        Q6=input('_n_ry:')
+        if Q6=='a_g':
+            print('correct')
+            q=q+1
+            i=i+1
+        else:
+            print('incorrect')
+            q=q+1       
+    elif q==7:
+        Q7=input('_y_hon:')
+        if Q7=='p_t':
+            print('correct')
+            q=q+1
+            i=i+1
+            
+        else:
+            print('incorrect')
+            q=q+1
+    elif q==8:
+        Q8=input('yo__u_e:')
+        if Q8=='ut_b':
+            print('correct')
+            q=q+1
+            i=i+1
+           
+        else:
+            print('incorrect')
+            q=q+1
+    elif q==9:
+        Q9=input('de_on:')
+        if Q9=='m':
+            print('correct')
+            q=q+1
+            i=i+1
+           
+        else:
+            print('incorrect')
+            q=q+1
+    elif q==10:
+        Q10=input('pa__ho_e:')
+        if Q10=='yp__n':
+            print('correct')
+            q=q+1
+            i=i+1
+          
+        else:
+            print('incorrect')
+            q=q+1
+    elif q==11:
+        print('恭喜完成挑戰!')
+        if i>=5:
+            print('你總共答對了',i,'題，你的英文能力真好!')
+        else:
+            print('你只答對',i,'題，不過沒關係，再接再厲!')
+        break
+    
+        
+            
+            
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
